7273a6c16f425836c3b00d609cb0531dcf61b13b
onyekachila <onyekachila@gmail.com>
2019-08-26 15:41:40 +0100

    Added functionality to consume the api from the console application

--- SureBank.ConsoleClient/Program.cs
+++ SureBank.ConsoleClient/Program.cs
@@ -1,5 +1,9 @@
 ﻿using IdentityModel.Client;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace SureBank.ConsoleClient
@@ -8,31 +12,59 @@ namespace SureBank.ConsoleClient
     {
         public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
 
-
         private static async Task MainAsync()
         {
-            // discover all end points using metadata of identity server
-
+            //discover all the endpoints using metadata of identity server
             var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
             if (disco.IsError)
             {
                 Console.WriteLine(disco.Error);
-                return; 
+                return;
             }
 
-            // Grab a bearer token
-            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            //Grab a bearer token
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secrets");
             var tokenResponse = await tokenClient.RequestClientCredentialsAsync("surebankapi");
 
             if (tokenResponse.IsError)
             {
                 Console.WriteLine(tokenResponse.Error);
-                return; 
+                return;
             }
 
             Console.WriteLine(tokenResponse.Json);
             Console.WriteLine("\n\n");
-        }
 
+            //Consume our Customer API
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var customerInfo = new StringContent(
+                JsonConvert.SerializeObject(
+                        new { Id = 10, FirstName = "Kachilala", LastName = "Stanley" }),
+                        Encoding.UTF8, "application/json");
+
+            var createCustomerResponse = await client.PostAsync("http://localhost:54072/api/customers/"
+                                                            , customerInfo);
+
+            if (!createCustomerResponse.IsSuccessStatusCode)
+            {
+                Console.WriteLine(createCustomerResponse.StatusCode);
+            }
+
+            var getCustomerResponse = await client.GetAsync("http://localhost:54072/api/customers/");
+            if (!getCustomerResponse.IsSuccessStatusCode)
+            {
+                Console.WriteLine(getCustomerResponse.StatusCode);
+            }
+            else
+            {
+                var content = await getCustomerResponse.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+
+            Console.Read();
+
+        }
     }
 }
