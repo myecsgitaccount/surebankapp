288af319b761c62a54fcdafb63f7ff7c9906712b
onyekachila <onyekachila@gmail.com>
2019-08-26 13:11:47 +0100

    Configured Identity Server to retrieve access token

---
+++ SureBank.IdenitySvr/Config.cs
@@ -0,0 +1,37 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SureBank.IdenitySvr
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetAllApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("surebankapi", "Customer api for sure bank")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secrets".Sha256())
+                    },
+                    AllowedScopes = { "surebankapi" }
+                }
+            }; 
+        }
+      
+    }
+}
